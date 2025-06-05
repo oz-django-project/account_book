56cb13554834ff5ef3f35db8c86c2f660468ca02
kyukyu300 <kyujin1252@gmail.com>
2025-06-06 03:41:00 +0900

    🐛 fix: migration 안되던 버그 수정
    ' ---------------------------------------
    ' 타입 종류
    ' - ✨feat:새로운 기능 추가
    ' - 🐛fix: 기능 수정, 버그 수정
    ' - 💡chore: 오타 수정 및 새로운 기능이 추가되지 않고, 코드가 변경 된 경우 (주석 추가 및 수정 포함)
    ' - 📝docs: 문서 수정 (readme 수정 시)
    ' - 🚚build : 빌드 관련 파일 수정 및 삭제한 경우
    ' - ✅test: 테스트 코드, 리팩터링 테스트 코드 추가(프로덕션 코드 변경 X)
    ' - ♻️refactor: 코드 리팩터링
    ' - 🚑hotfix : 긴급 수정
    '
    ' 제목 규칙
    ' * 제목 작성 후 공백 한줄을 포함해야, 제목과 본문이 구별됨
    ' * 제목 첫 글자는 대문자로 작성, 마침표를 사용하지 않음
    ' * 제목은 명령문으로 사용, 과거형을 사용하지 않음
    '
    ' 본문 규칙
    ' * 본문 작성 후 공백 한줄을 포함, 본문과 Resolves가 구별
    ' * 본문의 각 행은 72글자로 제한
    ' * 본문은 "왜"와 "무엇을"위주로 작성
    ' * 본문은 행으로 구분되어야 함

--- accounts/models.py
+++ accounts/models.py
@@ -139,8 +139,8 @@ class transaction_history(models.Model):
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     balance_after = models.DecimalField(max_digits=12, decimal_places=2)
     description = models.CharField(max_length=255)
-    transaction_type = models.CharField(max_length=5, choices=TRANSACTION_TYPE)
-    transfer_method = models.CharField(max_length=10, choices=TRANSACTION_METHOD)
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPE)
+    transfer_method = models.CharField(max_length=20, choices=TRANSACTION_METHOD)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
