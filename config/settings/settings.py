801afcf12a755e6e3fe5ba2c33fad0494ce83f43
kyukyu300 <kyujin1252@gmail.com>
2025-06-09 13:35:48 +0900

    🚚build: 데이터베이스 설정 변경
    ' ---------------------------------------
    ' 타입 종류
    ' - ✨feat:새로운 기능 추가
    ' - 🐛fix: 기능 수정, 버그 수정
    ' - 💡chore: 오타 수정 및 새로운 기능이 추가되지 않고, 코드가 변경 된 경우 (주석 추가 및 수정 포함)
    ' - 📝docs: 문서 수정 (readme 수정 시)
    ' - 🚚build : 빌드 관련 파일 수정 및 삭제한 경우
    ' - ✅test: 테스트 코드, 리팩터링 테스트 코드 추가(프로덕션 코드 변경 X)
    ' - ♻️refactor: 코드 리팩터링
    ' - 🚑hotfix : 긴급 수정
    '
    ' 제목 규칙
    ' * 제목 작성 후 공백 한줄을 포함해야, 제목과 본문이 구별됨
    ' * 제목 첫 글자는 대문자로 작성, 마침표를 사용하지 않음
    ' * 제목은 명령문으로 사용, 과거형을 사용하지 않음
    '
    ' 본문 규칙
    ' * 본문 작성 후 공백 한줄을 포함, 본문과 Resolves가 구별
    ' * 본문의 각 행은 72글자로 제한
    ' * 본문은 "왜"와 "무엇을"위주로 작성
    ' * 본문은 행으로 구분되어야 함

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(dotenv_path=BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -77,11 +79,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_mini',
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
 
