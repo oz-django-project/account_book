61abf54dda59a565ad6b9775ff0e753502850f86
kyukyu300 <kyujin1252@gmail.com>
2025-06-16 16:10:36 +0900

    ♻refactor: black, isort 실행
    ' ---------------------------------------
    ' 타입 종류
    ' - ✨feat:새로운 기능 추가
    ' - 🐛fix: 기능 수정, 버그 수정
    ' - 💡chore: 오타 수정 및 새로운 기능이 추가되지 않고, 코드가 변경 된 경우 (주석 추가 및 수정 포함)
    ' - 📝docs: 문서 수정 (readme 수정 시)
    ' - 🚚build : 빌드 관련 파일 수정 및 삭제한 경우
    ' - ✅test: 테스트 코드, 리팩터링 테스트 코드 추가(프로덕션 코드 변경 X)
    ' - ♻️refactor: 코드 리팩터링
    ' - 🚑hotfix : 긴급 수정
    '
    ' 제목 규칙
    ' * 제목 작성 후 공백 한줄을 포함해야, 제목과 본문이 구별됨
    ' * 제목 첫 글자는 대문자로 작성, 마침표를 사용하지 않음
    ' * 제목은 명령문으로 사용, 과거형을 사용하지 않음
    '
    ' 본문 규칙
    ' * 본문 작성 후 공백 한줄을 포함, 본문과 Resolves가 구별
    ' * 본문의 각 행은 72글자로 제한
    ' * 본문은 "왜"와 "무엇을"위주로 작성
    ' * 본문은 행으로 구분되어야 함

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.39.193.101', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ["3.39.193.101", "localhost", "127.0.0.1"]
 
 
 # Application definition
